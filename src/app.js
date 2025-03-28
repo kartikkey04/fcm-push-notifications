c0a604665a635afebe8db1a68e3fe282b2377191
kartikey <kartikkey0414@gmail.com>
2025-03-28 11:11:16 +0530

    middleware and routes added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 require("dotenv").config();
-
+const notificationRoutes = require("./routes/notificationRoutes");
 
 const app = express();
+
+app.use(express.json());
+app.use("/api", notificationRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
